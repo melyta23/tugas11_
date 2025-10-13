f8a96e8ae203c8bd71bed9824628b838c4b4363d
Mely Tri Andini <melytriandini@gmail.com>
2025-10-14 06:10:02 +0700

    komitan2

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ if (isset($_POST['login'])) {
     $username = mysqli_real_escape_string($koneksi, $_POST['username']);
     $password = md5($_POST['password']);  // md5, bisa diganti password_hash
 
-    $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $query = "SELECT * FROM user WHERE username='$username' AND password='$password'";
     $result = mysqli_query($koneksi, $query);
 
     if (mysqli_num_rows($result) > 0) {
